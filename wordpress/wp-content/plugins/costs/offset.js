b65b7d3736a5a19e8161794ce5f7b155f7a344cc
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-12-03 07:37:23 -0500

    Turn travel into an object

--- wordpress/wp-content/plugins/costs/offset.js
+++ wordpress/wp-content/plugins/costs/offset.js
@@ -1,5 +1,13 @@
 // offset.js
 
+function clearPlug() {
+    document.getElementById("xtra_offsetCalculator").innerHTML = "";
+}
+
+function setPlug(newPane) {
+    document.getElementById("xtra_offsetCalculator").appendChild(newPane);
+}
+
 class Travel {
     constructor() {
         this.page = 1;
@@ -12,84 +20,101 @@ class Travel {
 
     typePage() {
         clearPlug();
-        var offsetType = document.createElement('div');
-        var travelTypeHeader = document.createElement('h1');
-        travelTypeHeader.innerHTML = "How are you traveling?";
+        var pane = document.createElement('div');
+        var header = document.createElement('h1');
+        header.innerHTML = "How are you traveling?";
         var images = ["car.png", "plane.png", "train.png", "bus.png"];
         var tbl = document.createElement("table");
-        // creates a <tbody> element
         var tblBody = document.createElement("tbody");
         var count = 0;
-
         // creating all cells
         for (var i = 0; i < 2; i++) {
-
             // creates a table row
             var row = document.createElement("tr");
+            for (var j = 0; j < 2; j++) {
+                // Create a <td> element and a text node, make the text
+                // node the contents of the <td>, and put the <td> at
+                // the end of the table row
+                var cell = document.createElement("td");
+                var cellText = document.createElement("img");
+                cellText.src = "wp-content/plugins/costs/imgs/" + images[count];
+                callText.alt = images[count].substring(0, images[count].length - 2);
+                cellText.height = "50";
+                cellText.addEventListener("click", function(event) {
+                    alert(event.target);
+                    this.type = event.target.alt;
+                    subTypePage();
+                });
 
-        for (var j = 0; j < 2; j++) {
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-            var cell = document.createElement("td");
-            var cellText = document.createElement("img");
-            cellText.src = "wp-content/plugins/costs/imgs/" + images[count];
-            cellText.height = "50";
-            if(count == 0){
-                cellText.addEventListener("click", function(event){
-                    document.getElementById("xtra_offsetCalculator").innerHTML = "";
-                    document.getElementById("xtra_offsetCalculator").appendChild(carType).appendChild(buttons);
-                    page = 1.5;
-                });
-            } else if(count == 1){
-                cellText.addEventListener("click", function(event){
-                    document.getElementById("xtra_offsetCalculator").innerHTML = "";
-                });
-            } else if(count == 2){
-                cellText.addEventListener("click", function(event){
-                    document.getElementById("xtra_offsetCalculator").innerHTML = "";
-                    document.getElementById("xtra_offsetCalculator").appendChild(trainType).appendChild(buttons);
-                    page = 1.5;
-                });
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                count++;
             }
 
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            count++;
-        }
-
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
+            // add the row to the end of the table body
+            tblBody.appendChild(row);
         }
  
         // put the <tbody> in the <table>
         tbl.appendChild(tblBody);
 
-        offsetType.appendChild(travelTypeHeader);
-        offsetType.appendChild(tbl);
+        pane.appendChild(travelTypeHeader);
+        pane.appendChild(tbl);
         /*offsetType.appendChild(backButton);
         offsetType.appendChild(nextButton);*/
 
-        function generate_table() {
-        }
+        setPlug(pane);
     }
 
     subTypePage() {
+        clearPlug();
+        var pane = document.createElement('div');
+        var typeList = document.createElement("ul");
+        var paneHeader = document.createElement('h1');
+        paneHeader.innerHTML = "Could you be more specific?";
+        var cars = {
+            "car": ["Passenger", "Light Truck", "Motorcycle"],
+            "train": ["Intercity Rail", "Commuter Rail", "Transit Rail"],
+            "plane": ["<300 miles", "between 300 and 2300 miles", ">2300 miles"]
+        }
+        pane.appendChild(paneHeader);
+        pane.appendChild(carButtonTbl);
 
+        if (this.type == "bus") {
+            passengersPage();
+            return;
+        } else {
+            var subType, cellText;
+            for (item of cars[this.type]) {
+                subType = document.createElement("li");
+                cellText = document.createElement("input");
+                cellText.setAttribute("type", "radio");
+                cellText.setAttribute("name", item);
+
+                typeList.appendChild(cellText);
+            }
+            
+            var nextButton = document.createElement('span');
+            nextButton.className = "next round";
+            nextButton.innerHTML = "&#8250;";
+            nextButton.addEventListener("click", passengersPage);
+            pane.appendChild(nextButton);
+        }
+
+        setPlug(pane);
     }
 
     passengersPage() {
-
+        alert("We're working");
     }
 
     distancePage() {
 
     }
 }
-var page = 1;
 
-var buttons = document.createElement("div");
-
+new Travel();
+/*
 var backButton = document.createElement('span');
 backButton.className = "previous round";
 backButton.innerHTML = "&#8249;";
@@ -113,9 +138,8 @@ buttons.appendChild(backButton);
 buttons.appendChild(nextButton);
 
 
-/************************************/
+/************************************
 //type of transportation, page 1
-alert("YEP");
 var offsetType = document.createElement('div');
 var travelTypeHeader = document.createElement('h1');
 travelTypeHeader.innerHTML = "How are you traveling?";
@@ -126,7 +150,7 @@ generate_table();
 offsetType.appendChild(travelTypeHeader);
 offsetType.appendChild(tbl);
 /*offsetType.appendChild(backButton);
-offsetType.appendChild(nextButton);*/
+offsetType.appendChild(nextButton);*
 
 function generate_table() {
     // creates a <tbody> element
@@ -183,7 +207,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("xtra_offsetCalculator").appendChild(offsetType);
 });
 
-/***********************************/
+/***********************************
 //car type, page 1.5
 var carType = document.createElement('div');
 var carButtonTbl = document.createElement("table");
@@ -240,7 +264,7 @@ function generate_radiobuttons(type) {
     }
 }
 
-/*****************************************/
+/*****************************************
 //type of train, page 1.5
 var trainType = document.createElement('div');
 var trainButtonTbl = document.createElement("table");
@@ -254,7 +278,7 @@ var name = "trainType";
 generate_radiobuttons("train");
 trainType.appendChild(trainButtonTbl);
 
-/********************************************/
+/********************************************
 //distance traveled in airplane, page 1.5
 var airplaneType = document.createElement('div');
 var airplaneButtonTbl = document.createElement("table");
