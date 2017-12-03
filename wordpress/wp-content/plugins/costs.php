9a8d29f1d606eed6d449762d679ec2ad92eecf6c
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-12-03 06:33:37 -0500

    Test sample plugin

---
+++ wordpress/wp-content/plugins/costs.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name:   Costs of Carbon
+ * Description:   A carbon emissions calculator that calculates your carbon footprint so you know how much to offset it by
+ * Version:       0.1
+ * Author:        Extra Xtra
+ */
+class xtra_Costs_Caclulator extends WP_Widget {
+    // Set up the widget name and description.
+    public function __construct() {
+        $widget_options = array('classname' => 'costs_carbon_calculator',
+                                'description' => 'A carbon footprint offset calculator' );
+        parent::__construct( 'costs_carbon_calculator', 'Costs of Carbon Calculator', $widget_options );
+    }
+
+    // Create the widget output.
+    public function widget( $args, $instance ) {
+        $blog_title = get_bloginfo( 'name' );
+        $tagline = get_bloginfo( 'description' );
+        echo $args['before_widget'] . $args['before_title'] . $instance['funds_use'] . $args['after_title']; ?>
+        <p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
+        <p><strong>Tagline:</strong> <?php echo $tagline ?></p>
+        <?php echo $args['after_widget'];
+    }
+
+    // Create the admin area widget settings form.
+    public function form($instance) {
+        $funds_use = !empty( $instance['funds_use'] ) ? $instance['funds_use'] : ''; ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('funds_use'); ?>">Title:</label>
+            <input type="text" id="<?php echo $this->get_field_id('funds_use'); ?>" name="<?php echo $this->get_field_name('funds_use'); ?>" value="<?php echo esc_attr($funds_use); ?>" />
+        </p><?php
+    }
+
+    // Apply settings to the widget instance.
+    public function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['funds_use'] = strip_tags($new_instance['funds_use']);
+        return $instance;
+    }
+
+}
+// Register the widget.
+function xtra_register_costs_calculator() {
+  register_widget('xtra_Costs_Calculator');
+}
+add_action('widgets_init', 'xtra_register_costs_calculator');
+?>
